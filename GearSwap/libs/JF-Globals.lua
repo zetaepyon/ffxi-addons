b646a8ad0bd1e7be01138318ae6efa59d25b38ff
zetaepyon <zetaforums@theyarden.net>
2016-09-02 03:38:09 -0500

    Allow suppresion of add_to_chat for enhanced binding

--- GearSwap/libs/JF-Globals.lua
+++ GearSwap/libs/JF-Globals.lua
@@ -2,12 +2,12 @@ function define_global_sets()
 end
 
 function global_on_load()
-    bind('f9', 'gs c cycle OffenseMode', 'Cycle Offense Mode')
-    bind('!f9', 'gs c cycle RangedMode', 'Cycle Ranged Mode')
-    bind('@f9', 'gs c cycle WeaponskillMode', 'Cycle Weaponskill Mode')
-    bind('f10', 'gs c cycle DefenseMode', 'Cycle Defense Mode')
-    bind('f11', 'gs c cycle CastingMode', 'Cycle Casting Mode')
-    bind('f12', 'gs c cycle IdleMode', 'Cycle Idle Mode')
+    bind('f9', 'gs c cycle OffenseMode', 'Cycle Offense Mode', true)
+    bind('!f9', 'gs c cycle RangedMode', 'Cycle Ranged Mode', true)
+    bind('@f9', 'gs c cycle WeaponskillMode', 'Cycle Weaponskill Mode', true)
+    bind('f10', 'gs c cycle DefenseMode', 'Cycle Defense Mode', true)
+    bind('f11', 'gs c cycle CastingMode', 'Cycle Casting Mode', true)
+    bind('f12', 'gs c cycle IdleMode', 'Cycle Idle Mode', true)
 end
 
 function global_on_unload()
--- GearSwap/libs/JF-Utility.lua
+++ GearSwap/libs/JF-Utility.lua
@@ -3,10 +3,14 @@
 --------------------------------------------------------------------------------
 
 -- Enhanced keybind, including formatted chatlog output
-function bind(key, cmd, name, disp)
+function bind(key, cmd, name, hide)
+
+    hide = hide or true
+
     send_command('bind ' .. key .. ' ' .. cmd)
     key = key:upper():gsub('%^','Ctrl-'):gsub('%!','Alt-'):gsub('%@','Win-')
-    if not disp then
+
+    if not hide then
         add_to_chat(color.notice,name..': '..string.color(key,color.value))
     end
 end
