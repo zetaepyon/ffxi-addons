b5ea6dadfb742b349ec9c57322cf47ba722d1a44
zetaepyon <zetaforums@theyarden.net>
2016-09-13 01:37:47 -0500

    Refactor raw_register_event

--- GearSwap/libs/haste-tracker.lua
+++ GearSwap/libs/haste-tracker.lua
@@ -11,10 +11,17 @@ windower.raw_register_event('incoming chunk', function(id, data)
         local cat = action:get_category_string()
         local spell = action:get_spell()
 
-        -- Haste, Hastega (Spell), Refueling, Hastega (BP), Haste II, Hastega II, Erratic Flutter
-        local hastes = S{57,385,530,595,511,602,710}
+        local hastes = {
+            [57]  = 15, -- Haste
+            [385] = 15, -- Hastega (Spell)
+            [511] = 30, -- Haste II
+            [530] = 10, -- Refueling
+            [595] = 15, -- Hastega (BP)
+            [602] = 30, -- Hastega II
+            [710] = 30  -- Erratic Flutter
+        }
 
-        if S{'spell_finish','avatar_tp_finish'}:contains(cat) and hastes:contains(spell.id) then
+        if S{'spell_finish','avatar_tp_finish'}:contains(cat) and hastes[spell.id] then
 
             for target in action:get_targets() do
 
@@ -24,13 +31,7 @@ windower.raw_register_event('incoming chunk', function(id, data)
                 for act in target:get_actions() do act_info = act:get_basic_info() end
 
                 if target_name == player.name and act_info.param == 33 then
-                    if S{511,602,710}:contains(spell.id) then
-                        haste_type = 30
-                    elseif S{530}:contains(spell.id) then
-                        haste_type = 10
-                    else
-                        haste_type = 15
-                    end
+                    hastes = haste_vals[spell.id]
                 end
             end
         end
